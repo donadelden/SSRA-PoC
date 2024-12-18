433e5124a10ba8a253a731493d94dfb283c24262
donadelden <denisdonadel@gmail.com>
2024-12-18 18:06:28 +0100

    Creation of a test method

--- ssra/src/main.rs
+++ ssra/src/main.rs
@@ -3,7 +3,8 @@ use rabe::schemes::bsw::*;
 use rabe::utils::policy::pest::PolicyLanguage;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::env;
+use std::time::Duration;
+
 
 
 fn encrypt_cpabe(policy: &str, plaintext: &str) {
@@ -83,30 +84,106 @@ fn decrypt_cpabe() {
     println!("Decrypted data: {:?}", String::from_utf8(plaintext).expect("Unable to convert to string"));
 }
 
+
+fn test_everything() {
+
+    let attributes = &vec!["A", "B"];
+    let policy = r#""A" and "B""#; 
+
+    // Master keygen
+    let mut durations_masterk: Vec<Duration> = Vec::new();
+    let mut durations_userk: Vec<Duration> = Vec::new();
+    let mut durations_enc: Vec<Duration> = Vec::new();
+    let mut durations_dec: Vec<Duration> = Vec::new();
+    for _ in 0..100 {
+        let plaintext_bytes: Vec<u8> = (0..100).map(|_| rand::random::<u8>()).collect();
+
+        let start = Instant::now();
+        let (pk, msk) = setup();
+        let end = Instant::now();
+        durations_masterk.push(end - start);
+
+        let start = Instant::now();
+        let sk: CpAbeSecretKey = keygen(&pk, &msk, &attributes).unwrap();
+        let end = Instant::now();
+        durations_userk.push(end - start);
+
+        let start = Instant::now();
+        let ct_cp: CpAbeCiphertext = encrypt(&pk, &policy, PolicyLanguage::HumanPolicy, &plaintext_bytes).unwrap();
+        let end = Instant::now();
+        durations_enc.push(end - start);
+
+        let start = Instant::now();
+        let plaintext = decrypt(&sk, &ct_cp).unwrap();
+        let end = Instant::now();
+        durations_dec.push(end - start);
+
+        std::thread::sleep(Duration::from_secs(1));
+        println!("Iteration {} completed", _ + 1);
+    }
+
+
+    let mean_masterk: Duration = durations_masterk.iter().sum::<Duration>() / durations_masterk.len() as u32;
+    let stddev_masterk: f64 = (durations_masterk.iter().map(|d| {
+        let diff = *d - mean_masterk;
+        diff.as_secs_f64().powi(2)
+    }).sum::<f64>() / durations_masterk.len() as f64).sqrt();
+
+    let mean_userk: Duration = durations_userk.iter().sum::<Duration>() / durations_userk.len() as u32;
+    let stddev_userk: f64 = (durations_userk.iter().map(|d| {
+        let diff = *d - mean_userk;
+        diff.as_secs_f64().powi(2)
+    }).sum::<f64>() / durations_userk.len() as f64).sqrt();
+
+    let mean_enc: Duration = durations_enc.iter().sum::<Duration>() / durations_enc.len() as u32;
+    let stddev_enc: f64 = (durations_enc.iter().map(|d| {
+        let diff = *d - mean_enc;
+        diff.as_secs_f64().powi(2)
+    }).sum::<f64>() / durations_enc.len() as f64).sqrt();
+
+    let mean_dec: Duration = durations_dec.iter().sum::<Duration>() / durations_dec.len() as u32;
+    let stddev_dec: f64 = (durations_dec.iter().map(|d| {
+        let diff = *d - mean_dec;
+        diff.as_secs_f64().powi(2)
+    }).sum::<f64>() / durations_dec.len() as f64).sqrt();
+
+    println!("Mean Master Key Generation Time: {:?}", mean_masterk);
+    println!("Standard Deviation: {:?}", stddev_masterk);
+    println!("Mean User Key Generation Time: {:?}", mean_userk);
+    println!("Standard Deviation: {:?}", stddev_userk);
+    println!("Mean Encryption Time: {:?}", mean_enc);
+    println!("Standard Deviation: {:?}", stddev_enc);
+    println!("Mean Decryption Time: {:?}", mean_dec);
+    println!("Standard Deviation: {:?}", stddev_dec);   
+}
+
+
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <robot|tutor|user>", args[0]); 
-        std::process::exit(1);
-    }
-    let role = &args[1];
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() < 2 {
+    //     eprintln!("Usage: {} <robot|tutor|user>", args[0]); 
+    //     std::process::exit(1);
+    // }
+    // let role = &args[1];
 
-    match role.as_str() {
-        "robot" => {
-            encrypt_cpabe(r#""A" and "B""#, "dance like no one's watching, encrypt like everyone is!dance like no one's watching, encrypt like ev");
-        },
-        "tutor" => {
-            generate_master_keys();
-            generate_user_keys(vec!["A", "B"]); 
-        },
-        "user" => {
-            decrypt_cpabe();
-        },
-        _ => {
-            eprintln!("Invalid role: {}, should be robot, tutor, or user.", role);
-            std::process::exit(1);
-        }
-    }
+    // match role.as_str() {
+    //     "robot" => {
+    //         encrypt_cpabe(r#""A" and "B""#, "dance like no one's watching, encrypt like everyone is!dance like no one's watching, encrypt like ev");
+    //     },
+    //     "tutor" => {
+    //         generate_master_keys();
+    //         generate_user_keys(vec!["A", "B"]); 
+    //     },
+    //     "user" => {
+    //         decrypt_cpabe();
+    //     },
+    //     _ => {
+    //         eprintln!("Invalid role: {}, should be robot, tutor, or user.", role);
+    //         std::process::exit(1);
+    //     }
+    // }
+
+    test_everything();
 
 }
